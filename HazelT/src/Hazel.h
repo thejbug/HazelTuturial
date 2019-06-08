1a64b0b649412c828fff70e2c359c3a3913c3f20
JARED EMERY <u1092944@umail.utah.edu>
2019-06-08 15:17:25 -0600

    setup basic application entry point

---
+++ HazelT/src/Hazel.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//for use by hazel applications
+#include "Hazel/Application.h"
+
+//------------ENTRY POINT -------------
+#include "Hazel/EntryPoint.h"
+//-----------------------------------
